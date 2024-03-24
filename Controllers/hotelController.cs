a82c0a466df51b0ceb638aaf02ec9945362e7f5b
unknown <dev.juan.eloper@gmail.com>
2024-03-24 17:15:19 -0600

    Connect sql server | service

---
+++ Controllers/hotelController.cs
@@ -0,0 +1,22 @@
+﻿using API_HOSPITAL.Models.Hotel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Http;
+using static API_HOSPITAL.Models.Hotel.csObjectHotel;
+
+namespace API_HOSPITAL.Controllers
+{
+    public class hotelController : ApiController
+    {
+        // localhost:5000/rest/api/hotel
+        [HttpPost]
+        [Route("rest/api/createHotel")]
+        public IHttpActionResult createHotel (requestHotel model)
+        {
+            return Ok(new csHotel().createHotel(model.hotel_name, model.hotel_address, model.hotel_phone));
+        }
+    }
+}
---
+++ Models/Hotel/csHotel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace API_HOSPITAL.Models.Hotel
+{
+    public class csHotel
+    {
+        public csObjectHotel.responseHotel createHotel(string hotel_name, string hotel_address, string hotel_phone)
+        {
+            csObjectHotel.responseHotel response = new csObjectHotel.responseHotel();
+            // todo: ConfigurationManager es la palabra reservada para acceder a la configuración de la aplicación ( Web.config )
+            string connectionString = "";
+            SqlConnection sqlConnection = null;
+            try
+            {
+            connectionString = ConfigurationManager.ConnectionStrings["cnConnection"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string query = "INSERT INTO hotel (name, address, phone_number) VALUES (@hotel_name, @hotel_address, @hotel_phone)";
+            // todo: Se puede hacer de esta forma, pero es inseguro por inyecciones SQL ⬇️⬇️⬇️⬇️⬇️
+            //string query = "INSERT INTO hotel (name, address, phone_number) VALUES " + "( '" + hotel_name + "', '" + hotel_address + "', '" + hotel_phone + "' )";
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@hotel_name", hotel_name); // Esto se lo mandamos para evitar inyecciones SQL
+            sqlCommand.Parameters.AddWithValue("@hotel_address", hotel_address);
+            sqlCommand.Parameters.AddWithValue("@hotel_phone", hotel_phone);
+            response.status = sqlCommand.ExecuteNonQuery();
+            response.message = "Success";
+            }catch(Exception e)
+            {
+                response.status = 0;
+                response.message = "Failed to insert new Hotel: " + e.Message.ToString();
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            return response;
+        }
+    }
+}
